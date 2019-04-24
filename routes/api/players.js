ec7a076134b67f810623e3ffdef1b0f3db48faa7
Jordan Senken <jsenken@yahoo.com>
2019-04-23 21:58:58 -0400

    edits

--- routes/api/players.js
+++ routes/api/players.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const playersController = require("../../controllers/playersController");
 
-// Matches with "/api/players"
 router.route("/")
   .get(playersController.findAll)
   .post(playersController.create);
